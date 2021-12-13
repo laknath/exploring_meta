103434035c92f829c847183e1b5b4b03a1b0b31d
Laknath <blaknath@gmail.com>
2021-12-14 05:38:08 +1100

    add callback to setup env specific params

--- utils/env_maker.py
+++ utils/env_maker.py
@@ -10,11 +10,12 @@ mujoco_envs = ['HalfCheetahPT-v2', 'HopperPT-v2',
 metaworld_envs = ['ML1_reach-v1', 'ML1_pick-place-v1', 'ML1_push-v1', 'ML10', 'ML45']
 
 
-def _make_mujoco(env_name, n_workers):
+def _make_mujoco(env_name, n_workers, callback=None):
 
     def init_env():
         env = gym.make(env_name)
         env = ch.envs.ActionSpaceScaler(env)
+        if callback is not None: callback(env)
         return env
 
     if n_workers == 1:
@@ -23,36 +24,10 @@ def _make_mujoco(env_name, n_workers):
         return AsyncVectorEnv([init_env for _ in range(n_workers)])
 
 
-def _make_metaworld(env_name, n_workers, test, max_path_length):
-    if 'ML1_' in env_name:
-        env_name, task = env_name.split('_')
-    else:
-        task = False  # Otherwise, False corresponds to the sample_all argument
-
-    benchmark_env = getattr(utils, f"MetaWorld{env_name}")  # Use modded MetaWorld env
-
-    def init_env():
-        if test:
-            env = benchmark_env.get_test_tasks(task)
-        else:
-            env = benchmark_env.get_train_tasks(task)
-        if max_path_length is not None:
-            env.set_max_path_length(max_path_length)
-        env = ch.envs.ActionSpaceScaler(env)
-        return env
-
-    if n_workers == 1:
-        return init_env()
-    else:
-        return AsyncVectorEnv([init_env for _ in range(n_workers)])
-
-
-def make_env(env_name, n_workers, seed, test=False, max_path_length=None):
+def make_env(env_name, n_workers, seed, test=False, max_path_length=None, callback=None):
 
     if env_name in mujoco_envs:
-        env = _make_mujoco(env_name, n_workers)
-    elif env_name in metaworld_envs:
-        env = _make_metaworld(env_name, n_workers, test, max_path_length)
+        env = _make_mujoco(env_name, n_workers, callback=callback)
     else:
         raise NotImplementedError
 
